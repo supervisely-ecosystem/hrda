166e93547a85f60bb33487d7eb4747577d7e81a1
max-unfinity <poldersen2@gmail.com>
2023-07-13 20:07:31 +0000

    update main

--- src/main.py
+++ src/main.py
@@ -7,21 +7,19 @@ import src.ui.models as models
 import src.ui.classes as classes
 import src.ui.train_val_split as train_val_split
 # import src.ui.graphics as graphics
-# import src.ui.hyperparameters as hyperparameters
+import src.ui.hyperparameters as hyperparameters
 # import src.ui.train as train
 import src.ui.augmentations as augmentations
 import src.ui.handlers
-import src.ui.input_container as input_container
 
 
 widgets = [
-    input_container.card,
     input_project.card,
     models.card,
     classes.card,
     train_val_split.card,
     augmentations.card,
-    # hyperparameters.card,
+    hyperparameters.card,
     # train.card,
 ]
 layout = Container(widgets=widgets)
