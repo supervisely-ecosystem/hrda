ea423d1732550faf29b8175ef9a9f9346a0c4e82
max-unfinity <poldersen2@gmail.com>
2023-07-18 17:19:32 +0000

    fix SelectString in select dataset

--- src/ui/train_val_split.py
+++ src/ui/train_val_split.py
@@ -1,5 +1,5 @@
 import supervisely as sly
-from supervisely.app.widgets import TrainValSplits, Card, Select, Container, Field
+from supervisely.app.widgets import Card, SelectString, Container, Field
 import src.globals as g
 from src.ui.utils import HContainer
 
@@ -7,13 +7,13 @@ from src.ui.utils import HContainer
 
 dataset_infos = g.api.dataset.get_list(g.PROJECT_ID)
 dataset_names = [ds.name for ds in dataset_infos]
-dataset_items = [Select.Item(ds, ds.name) for ds in dataset_infos]
+# dataset_items = [Select.Item(ds, ds.name) for ds in dataset_infos]
 
-select_train_labeled = Select(dataset_items)
+select_train_labeled = SelectString(dataset_names)
 select_train_labeled_f = Field(select_train_labeled, "Source-domain train dataset (labeled)")
-select_train_unlabeled = Select(dataset_items)
+select_train_unlabeled = SelectString(dataset_names)
 select_train_unlabeled_f = Field(select_train_unlabeled, "Target-domain train dataset (unlabeled)")
-select_val_labeled = Select(dataset_items)
+select_val_labeled = SelectString(dataset_names)
 select_val_labeled_f = Field(select_val_labeled, "Target-domain val dataset (labeled)")
 
 
