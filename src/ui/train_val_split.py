c493ecdeae1e32b12272d2da0d83322c87478cbf
max-unfinity <poldersen2@gmail.com>
2023-07-18 14:06:39 +0000

    formatting

--- src/ui/train_val_split.py
+++ src/ui/train_val_split.py
@@ -1,6 +1,7 @@
 import supervisely as sly
 from supervisely.app.widgets import TrainValSplits, Card, Select, Container, Field
 import src.globals as g
+from src.ui.utils import HContainer
 
 # splits = TrainValSplits(project_id=g.PROJECT_ID)
 
@@ -19,18 +20,7 @@ select_val_labeled_f = Field(select_val_labeled, "Target-domain val dataset (lab
 card = Card(
     title="4️⃣ Select dataset",
     description="Define datasets for train and validation",
-    content=Container([
-        select_train_labeled_f,
-        select_train_unlabeled_f,
-        select_val_labeled_f
-    ], "horizontal", overflow="wrap"),
+    content=HContainer(
+        [select_train_labeled_f, select_train_unlabeled_f, select_val_labeled_f], overflow="wrap"
+    ),
 )
-
-
-# def dump_train_val_splits(project_dir):
-#     # dump splits into json files
-#     splits._project_fs = sly.Project(project_dir, sly.OpenMode.READ)
-#     train_split, val_split = splits.get_splits()
-#     app_dir = g.app_dir
-#     sly.json.dump_json_file(train_split, f"{app_dir}/train_split.json")
-#     sly.json.dump_json_file(val_split, f"{app_dir}/val_split.json")
