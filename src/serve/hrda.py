edbbaabdf99fe31dd35274ec9b0182d269be7ce7
max-unfinity <poldersen2@gmail.com>
2023-08-03 09:58:50 +0000

    fix serve

--- src/serve/hrda.py
+++ src/serve/hrda.py
@@ -1,9 +1,11 @@
+import os
 from typing import Any, Dict, List, Literal
 from typing_extensions import Literal
 import torch
 import numpy as np
 
 from supervisely.nn.inference import SemanticSegmentation
+from supervisely.nn.inference.gui import InferenceGUI
 from supervisely.nn.prediction_dto import PredictionSegmentation
 import supervisely as sly
 
@@ -19,7 +21,7 @@ class HRDA(SemanticSegmentation):
         model_dir: str,
         device: Literal["cpu", "cuda", "cuda:0", "cuda:1", "cuda:2", "cuda:3"] = "cpu",
     ):
-        config_path, weights_path = self.download_model_files(model_dir)
+        config_path, weights_path = self.download_model_files()
         self.load_model(config_path, weights_path, device)
         print(f"✅ Model has been successfully loaded on {device.upper()} device")
 
@@ -41,23 +43,8 @@ class HRDA(SemanticSegmentation):
         self._model_meta = sly.ProjectMeta(obj_classes=sly.ObjClassCollection(obj_classes))
         self.device = device
 
-    def download_model_files(self, model_dir: str):
-        if self.gui is not None:
-            model_source = self.gui.get_model_source()
-            if model_source == "Pretrained models":
-                selected_model = self.gui.get_checkpoint_info()
-                weights_path, config_path = self.download_pretrained_files(
-                    selected_model, model_dir
-                )
-            elif model_source == "Custom models":
-                custom_weights_link = self.gui.get_custom_link()
-                weights_path, config_path = self.download_custom_files(
-                    custom_weights_link, model_dir
-                )
-        return config_path, weights_path
-
     def get_classes(self) -> List[str]:
-        return self.class_names  # e.g. ["cat", "dog", ...]
+        return self.class_names
 
     def get_info(self) -> dict:
         info = super().get_info()
@@ -70,7 +57,30 @@ class HRDA(SemanticSegmentation):
     def predict(
         self, image_path: str, settings: Dict[str, Any] = None
     ) -> List[PredictionSegmentation]:
-        # prepare data
+        # prepare image
+        data = self.prepare_image(image_path)
+        
+        # we will unpad and rescale later
+        img_meta = data["img_metas"][0][0]
+        ori_shape = img_meta["ori_shape"]
+        if img_meta.get("pad_shape"):
+            data["rescale"] = False
+
+        # predict
+        with torch.no_grad():
+            result = self.model(return_loss=False, **data)
+
+        # unpad and resize manually
+        if img_meta.get("pad_shape"):
+            h, w, c = img_meta["img_shape"]
+            dtype = result[0].dtype
+            result = result[0][:h, :w].astype(np.uint8)
+            result = mmcv.imresize(result, ori_shape[:2][::-1], interpolation="nearest")
+            result = result.astype(dtype)
+
+        return [sly.nn.PredictionSegmentation(result)]
+
+    def prepare_image(self, image_path: str) -> dict:
         data = dict(img=image_path)
         data = self.test_pipeline(data)
         data = collate([data], samples_per_gpu=1)
@@ -79,31 +89,28 @@ class HRDA(SemanticSegmentation):
             data = scatter(data, [self.device])[0]
         else:
             data["img_metas"] = [i.data[0] for i in data["img_metas"]]
+        return data
 
-        img_meta = data["img_metas"][0][0]
-        ori_shape = img_meta["ori_shape"]
-        if img_meta.get("pad_shape"):
-            # we will unpad and rescale later
-            data["rescale"] = False
+    def download_model_files(self):
+        if self.gui is not None:
+            model_source = self.gui.get_model_source()
+            if model_source == "Pretrained models":
+                # selected_model = self.gui.get_checkpoint_info()
+                # weights_path, config_path = self.download_pretrained_files(
+                #     selected_model, model_dir
+                # )
+                pass
+            elif model_source == "Custom models":
+                custom_weights_link = self.gui.get_custom_link()
+                weights_path = self.download(custom_weights_link)
+                remote_config_path = os.path.dirname(custom_weights_link)+"/config.py"
+                config_path = self.download(remote_config_path)
+            return config_path, weights_path
 
-        ### DEBUG
-        # input_shape = data['img'][0].shape
-        # print("")
-        # print(f"{ori_shape=}, {input_shape=}")
-        ###
-
-        with torch.no_grad():
-            result = self.model(return_loss=False, **data)
-
-        # debug
-        # print(f"{result[0].shape=}")
-
-        if img_meta.get("pad_shape"):
-            # unpad and resize manually
-            h, w, c = img_meta["img_shape"]
-            dtype = result[0].dtype
-            result = result[0][:h, :w].astype(np.uint8)
-            result = mmcv.imresize(result, ori_shape[:2][::-1])
-            result = result.astype(dtype)
-
-        return [sly.nn.PredictionSegmentation(result)]
+    def initialize_gui(self) -> None:
+        self._gui = InferenceGUI(
+            [],
+            self.api,
+            support_pretrained_models=False,
+            support_custom_models=True,
+        )
--- src/serve/test.py
+++ src/serve/test.py
@@ -2,8 +2,6 @@ import os
 from dotenv import load_dotenv
 from src.serve.hrda import HRDA
 import supervisely as sly
-from supervisely.nn import PredictionSegmentation
-import mmcv
 from src.sly_utils import mask2annotation
 
 load_dotenv(os.path.expanduser("~/supervisely.env"))
@@ -18,17 +16,7 @@ model.load_model(config_path, weights_path, device)
 
 pred = model.predict(img_path)
 
-
-def to_annotation(pred: PredictionSegmentation):
-    b = sly.Bitmap(pred)
-    obj_class = sly.ObjClass(pred.class_name, sly.Bitmap)
-    l = sly.Label(b, obj_class)
-    ann = sly.Annotation(pred.mask.shape, [l])
-    return ann
-
-
 ann = mask2annotation(pred[0].mask, model.class_names, model.model.PALETTE)
-# ann = to_annotation(pred)
 
 img = sly.image.read(img_path)
 ann.draw_pretty(img)
