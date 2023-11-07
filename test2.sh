573888e318689942d874d5a72e5375a2a2efc3af
unfinity <poldersen2@gmail.com>
2023-11-07 15:36:30 +0000

    inference notebook added

--- test2.sh
+++
@@ -1,2 +0,0 @@
-rm -rf preds_v2_test
-python -m tools.test configs/hrda/cracks_cfg_test.py work_dirs/local-basic/230705_1818_hrda_v2_e28bd/iter_25000.pth --eval mIoU --show-dir preds_v2_test --opacity 0.4
