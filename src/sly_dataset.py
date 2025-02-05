c06b4296e75f489a53ad620c99e92a0857a43756
vorozhkog <vorozhkog@icloud.com>
2025-02-05 12:59:58 +0000

    fix logger

--- src/sly_dataset.py
+++ src/sly_dataset.py
@@ -99,8 +99,7 @@ def convert_project_masks(project_fs: sly.Project, ann_dir="seg2"):
     if len(converted_images) > 0:
         unsupported_exts = [os.path.splitext(k)[1] for k in converted_images.keys()]
         sly.logger.info(
-            "Found unsupported image extensions: "
-            + ", converted to jpg:".join(set(unsupported_exts)),
+            "Found unsupported image extensions: " + ", ".join(set(unsupported_exts)),
             extra=converted_images,
         )
     sly.logger.info("project masks converted")
